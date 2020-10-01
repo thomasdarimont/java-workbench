2650a4347f638a3bc7fd2cf6b35c4ec9d92161e7
Thomas Darimont <thomas.darimont@googlemail.com>
2020-10-01 02:40:04 +0200

    Revise VirtualThreadsDemo

--- java16-loom/src/main/java/wb/java16loom/VirtualThreadsDemo.java
+++ java16-loom/src/main/java/wb/java16loom/VirtualThreadsDemo.java
@@ -1,24 +1,44 @@
 package wb.java16loom;
 
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 
 class VirtualThreadsDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        var executorService = Executors.newFixedThreadPool(2);
+        int carrierThreads = 2;
+
+        var executorService = Executors.newFixedThreadPool(carrierThreads, r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        int nTasks = 10;
+        CountDownLatch cdl = new CountDownLatch(nTasks);
+
+        Runnable task = () -> VirtualThreadsDemo.sleep(cdl);
 
         var threadBuilder = Thread.builder()
-                .name("demo-thread", 1)
+                .name("demo-thread-", 1)
+                .daemon(true)
                 .virtual(executorService)
-                .task(VirtualThreadsDemo::task);
+                .task(task);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nTasks; i++) {
             threadBuilder.build().start();
+//            executorService.execute(task);
         }
+
+        System.out.println("Waiting for task completion");
+        cdl.await();
+        executorService.shutdown();
+        System.out.println("Terminated");
     }
 
-    static void task() {
+    static void task(CountDownLatch cdl) {
 
         System.out.printf("Started %s%n", Thread.currentThread().getName());
 
@@ -27,10 +47,27 @@ class VirtualThreadsDemo {
             iters++;
 
             if (iters % 10000 == 0) {
+                Thread.yield();
+
                 System.out.printf("Running in %s%n", Thread.currentThread().getName());
             }
 
-            Thread.yield();
+            if (iters > 100000) {
+                break;
+            }
         }
+
+        cdl.countDown();
+    }
+
+    static void sleep(CountDownLatch cdl) {
+        System.out.printf("Going to sleep %s%n", Thread.currentThread().getName());
+        try {
+            Thread.sleep(Duration.ofSeconds(1));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Woke up %s%n", Thread.currentThread().getName());
+        cdl.countDown();
     }
 }
