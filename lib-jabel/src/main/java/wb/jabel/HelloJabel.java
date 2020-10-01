4a1e3a8480d645463156736004eb399970ccb3db
Thomas Darimont <thomas.darimont@googlemail.com>
2020-10-01 02:01:29 +0200

    Add jabel example project

---
+++ lib-jabel/src/main/java/wb/jabel/HelloJabel.java
@@ -0,0 +1,18 @@
+package wb.jabel;
+
+/**
+ * <pre>{@code
+ * java8
+ *
+ * java -cp target/classes wb.jabel.HelloJabel
+ * }</pre>
+ */
+class HelloJabel {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                        Hello Jabel with Multi-line Strings
+                """);
+    }
+}
